02027be9b764bfbe8e1547029ec3a44dd5c8787f
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2022-01-17 21:50:23 +0000

    Completed canvas task and added testing for displayed stats

--- Car Customizer/Car.swift
+++ Car Customizer/Car.swift
@@ -13,4 +13,14 @@ struct Car {
     var topSpeed: Int
     var acceleration: Double
     var handling: Int
+    
+    func displayStats() -> String {
+            return """
+                Make: \(make)
+                Model: \(model)
+                Top Speed: \(topSpeed)mph
+                Acceleration (0-60): \(acceleration)s
+                Handling: \(handling)
+                """
+        }
 }
--- Car CustomizerTests/Car_CustomizerTests.swift
+++ Car CustomizerTests/Car_CustomizerTests.swift
@@ -23,10 +23,18 @@ class Car_CustomizerTests: XCTestCase {
     }
 
     func testCarCorrectlyReturnsAttributes() {
-        //arrange
-        //act
-        //???
-       
-    }
-
+        let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+        let expected = """
+        Make: Mazda
+        Model: MX-5
+        Top Speed: 125mph
+        Acceleration (0-60): 7.7s
+        Handling: 5
+        """
+        let actual = car.displayStats()
+        XCTAssertEqual(expected, actual)
+        }
+    
 }
+
+
