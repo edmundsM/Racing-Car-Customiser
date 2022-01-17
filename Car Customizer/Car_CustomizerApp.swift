7db0101fab886eebf6ce79003c970e1ecf882ef2
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2022-01-17 19:16:09 +0000

    Initial Commit

---
+++ Car Customizer/Car_CustomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Car_CustomizerApp.swift
+//  Car Customizer
+//
+//  Created by Max Edmunds on 17/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Car_CustomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Car CustomizerUITests/Car_CustomizerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Car_CustomizerUITestsLaunchTests.swift
+//  Car CustomizerUITests
+//
+//  Created by Max Edmunds on 17/01/2022.
+//
+
+import XCTest
+
+class Car_CustomizerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
