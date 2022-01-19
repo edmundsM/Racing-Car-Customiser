8506dfafc7ed785a24662eaa0116927c34045405
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2022-01-19 21:45:25 +0000

    Finished part 2 including remaining funds

--- Car Customizer/ContentView.swift
+++ Car Customizer/ContentView.swift
@@ -14,6 +14,23 @@ struct ContentView: View {
     @State private var tiresPackage = false
     @State private var nitroPackage = false
     @State private var launchPackage = false
+    @State private var remainingFunds = 1000
+    
+    var exhaustPackageEnabled: Bool {
+        return exhaustPackage ? true : remainingFunds >= 500 ? true : false
+    }
+    
+    var tiresPackageEnabled: Bool {
+        return tiresPackage ? true : remainingFunds >= 500 ? true : false
+    }
+    
+    var nitroPackageEnabled: Bool {
+        return nitroPackage ? true : remainingFunds >= 500 ? true : false
+    }
+    
+    var launchPackageEnabled: Bool {
+        return launchPackage ? true : remainingFunds >= 500 ? true : false
+    }
     
     var body: some View {
         
@@ -23,8 +40,10 @@ struct ContentView: View {
                 self.exhaustPackage = newValue
                 if newValue == true {
                     starterCars.cars[selectedCar].topSpeed += 5
+                    remainingFunds -= 500
                 } else {
                     starterCars.cars[selectedCar].topSpeed -= 5
+                    remainingFunds += 500
                 }
             }
         )
@@ -34,8 +53,10 @@ struct ContentView: View {
                 self.tiresPackage = newValue
                 if newValue == true {
                     starterCars.cars[selectedCar].handling += 2
+                    remainingFunds -= 500
                 } else {
                     starterCars.cars[selectedCar].handling -= 2
+                    remainingFunds += 500
                 }
             }
         )
@@ -45,8 +66,10 @@ struct ContentView: View {
                 self.nitroPackage = newValue
                 if newValue == true {
                     starterCars.cars[selectedCar].topSpeed += 10
+                    remainingFunds -= 500
                 } else {
                     starterCars.cars[selectedCar].topSpeed -= 10
+                    remainingFunds += 500
                 }
             }
         )
@@ -56,34 +79,46 @@ struct ContentView: View {
                 self.launchPackage = newValue
                 if newValue == true {
                     starterCars.cars[selectedCar].acceleration -= 1.5
+                    starterCars.cars[selectedCar].handling += 1
+                    remainingFunds -= 1000
                 } else {
                     starterCars.cars[selectedCar].acceleration += 1.5
+                    starterCars.cars[selectedCar].handling += 1
+                    remainingFunds += 1000
                 }
             }
         )
         
         
         
-        
-        Form {
-            VStack(alignment: .leading, spacing: 20) {
-                Text(starterCars.cars[selectedCar].displayStats())
-                Button("Random Car", action: {
-                    selectedCar = Int.random(in: 0 ..< self.starterCars.cars.count)})
-                Button("Next Car", action: {
-                    if selectedCar == 3 {
-                        selectedCar = 0
-                    } else {
-                        selectedCar += 1
-                    }
-                })
+        VStack {
+            Form {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text(starterCars.cars[selectedCar].displayStats())
+                    Button("Random Car", action: {
+                        selectedCar = Int.random(in: 0 ..< self.starterCars.cars.count)})
+                    Button("Next Car", action: {
+                        if selectedCar == 3 {
+                            selectedCar = 0
+                        } else {
+                            selectedCar += 1
+                        }
+                    })
+                }
+                Section {
+                    Toggle("Exhaust Package (Cost: 500)", isOn: exhaustPackageBinding)
+                        .disabled(!exhaustPackageEnabled)
+                    Toggle("Tires Package (Cost: 500)", isOn: tiresPackageBinding)
+                        .disabled(!tiresPackageEnabled)
+                    Toggle("Nitro Boost Package (Cost: 500)", isOn: nitroPackageBinding)
+                        .disabled(!nitroPackageEnabled)
+                    Toggle("Launch Control Package (Cost: 1000)", isOn: launchPackageBinding)
+                        .disabled(!launchPackageEnabled)
+                }
             }
-            Section {
-                Toggle("Exhaust Package", isOn: exhaustPackageBinding)
-                Toggle("Tires Package", isOn: tiresPackageBinding)
-                Toggle("Nitro Boost Package", isOn: nitroPackageBinding)
-                Toggle("Launch Control Package", isOn: launchPackageBinding)
-            }
+            Text("Remaining Funds: \(remainingFunds)")
+                .foregroundColor(.red)
+                .baselineOffset(20)
         }
     }
 }
